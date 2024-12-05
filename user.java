e3519a00f055b0bff67d8bb4bfcfd44c03d24b28
unik09 <139422757+unik09@users.noreply.github.com>
2024-12-05 18:00:14 +0300

    Update user.java
    user class

--- user.java
+++ user.java
@@ -1,1 +1,15 @@
-public class
+public class user{
+  int userId;
+  String name, email, password, role, bio;
+
+    User(int userId, String name, String email, String password, String role, String bio) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.bio= bio;
+    }
+  
+
+}
