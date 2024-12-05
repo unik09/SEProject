33f81b9bdb7284f1ef10d06b1b99b207987b01ec
unik09 <139422757+unik09@users.noreply.github.com>
2024-12-05 20:48:36 +0300

    Update PmuMentorSystem.java
    main method and creating account

--- PmuMentorSystem.java
+++ PmuMentorSystem.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class PmuMentorSystem {
+     static ArrayList<User> users = new ArrayList<>();
+    static int userIdCounter = 001;
     
+
+    //Main method
+     public static void main(String[] args) {
+        createAccount(); // Call the method
+    }
+
+
+
+
+
+    // creating new account method
+    public static void createAccount() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Enter Password: ");
+        String password = sc.nextLine();
+
+        // Check if email is already taken
+        for (User user : users) {
+            if (user.email.equals(email)) {
+                System.out.println("Error: Email already exists!");
+                return;
+            }
+
+             User newUser = new User(userIdCounter++, name, email, password, role);
+        users.add(newUser);
+
+        System.out.println("Account created successfully!");
     
-   
 }
